14f0f863175636814635bdb4b53c09a18e53c999
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-04-07 13:47:45 -0600

    background image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import PicCard from './components/PicCard';
 import CertCard from './components/CertCard';
 import Footer from './components/Footer';
+import background from "./images/black-background.jpg";
 import initFontAwesome from "./initFontAwesome";
 initFontAwesome();
 
@@ -11,9 +12,12 @@ function App() {
   return (
     <div>
       <Header />
-        <div className='mainBody'>
+        
+          <div className='bgImg' style={{ backgroundImage: `url(${background})` }}>
+          <div className='mainBody'>
             <PicCard />
             <CertCard />
+            </div>
         </div>
       <Footer />
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,42 @@
 import './Footer.css';
-import { FaGithub, FaLinkedin, FaFacebook, FaReddit, FaPhoneAlt, FaRegEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaFacebook, FaPhoneAlt, FaRegEnvelope } from 'react-icons/fa';
 import { MDBBtn } from 'mdb-react-ui-kit';
+import ReactLogo from '../../src/images/logo512.png';
 
 const Footer = () => {
     return (
         <footer className='footer'>
-            <MDBBtn className='m-1' style={{ backgroundColor: '#66C40E' }} href='#'>
-                <FaPhoneAlt className='icon' />
-            </MDBBtn>
-            <MDBBtn className='m-1' style={{ backgroundColor: '#C40E0E' }} href='#'>
-                <FaRegEnvelope className='icon' />
-            </MDBBtn>
-            <MDBBtn className='m-1' style={{ backgroundColor: '#0082ca' }} href='#'>
-                <FaLinkedin className='icon' />
-            </MDBBtn>
-            <MDBBtn className='m-1' style={{ backgroundColor: '#333333' }} href='#'>
-                <FaGithub className='icon' />
-            </MDBBtn>
-            <MDBBtn className='m-1' style={{ backgroundColor: '#3b5998' }} href='#'>
-                <FaFacebook className='icon' />
-            </MDBBtn>
-            <MDBBtn className='m-1' style={{ backgroundColor: '#ff4500' }} href='#'>
-                <FaReddit className='icon' />
-            </MDBBtn>
-            
-            
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='contactTitle'>
+                            <h2>Contact</h2>
+                        </div>
+                        <div className='contact'>
+                            <MDBBtn className='m-1' style={{ backgroundColor: '#66C40E' }} href='#'>
+                                <FaPhoneAlt className='icon' />
+                            </MDBBtn>
+                            <MDBBtn className='m-1' style={{ backgroundColor: '#C40E0E' }} href='#'>
+                                <FaRegEnvelope className='icon' />
+                            </MDBBtn>
+                            <MDBBtn className='m-1' style={{ backgroundColor: '#0082ca' }} href='#'>
+                                <FaLinkedin className='icon' />
+                            </MDBBtn>
+                            <MDBBtn className='m-1' style={{ backgroundColor: '#333333' }} href='#'>
+                                <FaGithub className='icon' />
+                            </MDBBtn>
+                            <MDBBtn className='m-1' style={{ backgroundColor: '#3b5998' }} href='#'>
+                                <FaFacebook className='icon' />
+                            </MDBBtn>
+                        </div>
+                    </div>
+                    <div className='col'>
+                        <div className='sig-container'>
+                            <p className='sig'>This website was created by Derek Ewell using React.js <img src={ReactLogo} className='rLogo' alt='The React logo' /></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
