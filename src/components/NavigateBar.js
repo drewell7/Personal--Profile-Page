85e52e849a83e1d0178f42656346426459f9bd61
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-04-13 22:40:35 -0600

    work on navbar

--- src/components/NavigateBar.js
+++ src/components/NavigateBar.js
@@ -1,14 +1,37 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './Nav.css';
+import DElogo from '../../src/images/DELogob.png';
+import { FaChevronDown } from "react-icons/fa";
 
 const NavigateBar = () => {
     return (
-        <Navbar sticky={'top'} >
-             <NavbarBrand>
-
-             </NavbarBrand>
-    
+        <Navbar sticky={'top'} dark color="dark">
             
+            <NavbarBrand>
+                <img src={DElogo} width="60" height="60" className='DEIcon' alt="" />
+            </NavbarBrand>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-6'>
+                        </div>
+                        <div className='col downLinks'>
+                            <h4 className='downLinkTitle'>BIO</h4>
+                            <FaChevronDown className='downIcon' />
+                        </div>
+                        <div className='col downLinks'>
+                            <h4 className='downLinkTitle'>SKILLS</h4>
+                            <FaChevronDown className='downIcon' />
+                        </div>
+                        <div className='col downLinks'>
+                            <h4 className='downLinkTitle'>PROJECTS</h4>
+                            <FaChevronDown className='downIcon' />
+                        </div>
+                        <div className='col downLinks'>
+                            <h4 className='downLinkTitle'>CONTACT</h4>
+                            <FaChevronDown className='downIcon' />
+                        </div>
+                    </div>
+                </div>
             </Navbar>
     );
 };
